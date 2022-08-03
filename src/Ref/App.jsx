ff8f756f02be8fedb61126d174576209a7c37858
Akshay Neje <akshayneje2823@gmail.com>
2022-08-03 20:47:29 +0530

    What is Ref

---
+++ src/Ref/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Refs from './Refs'
+
+function App() {
+  return (
+    <div>
+        <Refs/>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './Expense Tracker/App';
+import App from './Ref/App';
 
 
 ReactDOM.render(
